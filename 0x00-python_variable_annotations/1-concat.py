7ac52b5719d45b6781ba924d8b0e512208c38cd4
Chekwasy <richardchekwas@gmail.com>
2023-12-08 07:19:37 +0100

    aa

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
-'''Task 1's module.
-'''
+"""add two string with anotate"""
 
 
 def concat(str1: str, str2: str) -> str:
-    '''Concatenates two strings.
-    '''
+    """the function"""
+
     return str1 + str2
