526ddfe077b78e8732ef696ed25edfbb305a8bda
Chekwasy <richardchekwas@gmail.com>
2023-12-08 07:13:00 +0100

    aa

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
-'''Task 0's module.
-'''
+""" add a and b with anotate """
 
 
 def add(a: float, b: float) -> float:
-    '''Adds two floating-point numbers.
-    '''
     return a + b
