f36e1692d2c020d2537db3fbfdd1715a03f17b9e
Chekwasy <richardchekwas@gmail.com>
2023-12-08 21:29:32 +0100

    aa

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -2,5 +2,7 @@
 """anotate certain variable"""
 
 
-def diff(a: int = 1, pi: float = 3.14, i_understand_annotations: bool = True, school: str = "Holberton") -> None:
-    """the function"""
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
